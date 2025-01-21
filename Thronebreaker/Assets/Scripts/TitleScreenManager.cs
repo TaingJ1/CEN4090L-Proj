9210fa466b5b0e3238b091447890c03fd69dffb1
fernthefloragato <gclewis16@gmail.com>
2025-01-21 02:08:49 -0500

    Scenes, Folders, and Buttons on Title
    - Added organization folders
    - Added Basic Scenes
    - Added 3 buttons on main menu that directs to other scenes
        - includes a title manager script that just basically acts as functions that tell the button what to do when clicked

---
+++ Thronebreaker/Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;      // Allows the script to change scenes
+
+
+public class Title : MonoBehaviour
+{
+    //[SerializeField] private string
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);      // Loads the overworld scene for now
+    }
+
+    public void Quit()        // Ends the current playing session of the game
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
